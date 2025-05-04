f5471f2a093622c4fd9db77fa85d3ca64d874eae
tanlee102 <lequoctanare@gmail.com>
2025-05-04 12:50:05 +0700

    first commit

--- app/api/route.js
+++ app/api/route.js
@@ -19,10 +19,7 @@ export async function GET(request) {
 
   try {
     let browser;
-    if (
-      process.env.NODE_ENV === "production" ||
-      process.env.VERCEL_ENV === "production"
-    ) {
+    if (false) {
       const executablePath = await chromium.executablePath(
         "https://github.com/Sparticuz/chromium/releases/download/v131.0.1/chromium-v131.0.1-pack.tar"
       );
