c26814434cc1538bdd9bf4dda4c384f2363fa0fe
tanlee102 <lequoctanare@gmail.com>
2025-05-04 15:08:33 +0700

    1

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -1,5 +1,5 @@
-// const { join } = require("path");
-// /** @type {import('puppeteer').Configuration} */
-// module.exports = {
-//   cacheDirectory: join(__dirname, ".cache", "puppeteer"),
-// };
+const { join } = require("path");
+/** @type {import('puppeteer').Configuration} */
+module.exports = {
+  cacheDirectory: join(__dirname, ".cache", "puppeteer"),
+};
